e90cebd5a7ea181f4d6cdd3411e8888ffa206be5
yoheitaonishi <onishiyoheita@gmail.com>
2018-05-07 05:51:23 +0100

    .gitignore is now working

--- script/order.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# KuCoin production
-cd /var/www/Cheetah/src && /home/ec2-user/.pyenv/versions/anaconda3-5.1.0/bin/python apply_exchanges.py
-# KuCoin dev
-#cd /Users/yoheitaonishi/block_chain/bot/Cheetah/src && python apply_exchanges.py
-
